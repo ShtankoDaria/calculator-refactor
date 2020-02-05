658fe26af51e62a5f6b02f67e98b041cf432a1d5
ShtankoDaria <daria.shtanko@gmail.com>
2020-02-05 17:25:04 +0100

    Handler

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,12 +1,15 @@
 function calculateHandler(event) {
   // read & process user input from event
 
+  var a = parseInt(document.querySelector('#value1').value);
+  var b = parseInt(document.querySelector('#value2').value);
+  var operator = document.querySelector('#operator').value;
   // pass user input through core logic
 
   // render output to DOM for user
 
   // log user action for developers
-
+calculate.log(calculate)
   // return true for the browser
   return true;
 }
