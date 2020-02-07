f3659fab1cc9a20e30a1bf70001a82353198e4c6
ShtankoDaria <daria.shtanko@gmail.com>
2020-02-07 14:34:26 +0100

    Listener

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,10 +1,3 @@
-<<<<<<< Updated upstream
-document.getElementById('calculate')
-  .addEventListener('click', function calculateHandler(event) {
-    console.log(event);
-  });
 
-=======
 document.getElementById('calculate-button')
   .addEventListener('click', calculateHandler);
->>>>>>> Stashed changes
