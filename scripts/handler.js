1b020bfe55bc127946b8f4b14566d57b4dedbd6b
ShtankoDaria <daria.shtanko@gmail.com>
2020-02-07 12:57:22 +0100

    Handler changes

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,24 +1,18 @@
 function calculateHandler(event) {
   // read & process user input from event
-
-  var a = parseInt(document.querySelector('#value1').value);
-  var b = parseInt(document.querySelector('#value2').value);
-  var operation = document.querySelector('#operator').value;
-  var calculate;
+  const form = event.target.form;
+  const value1 = parseInt(form.value1.value);
+  const value2 = parseInt(form.value2.value);
+  const operator = form.operator.value;
   // pass user input through core logic
-  if (typeof operation !== 'string') {
-    throw new Error('operation should be a string');
-  }
-  if (typeof a !== 'number') {
-    throw new Error('a should be a number');
-  }
-  if (typeof b !== 'number') {
-    throw new Error('b should be a number');
-  }
+  const result = doMath(value1, value2, operator)
   // render output to DOM for user
-  document.querySelector('#result').innerHTML = calculate;
+  document.querySelector('#results').innerHTML = calculate;
   // log user action for developers
-calculate.log(calculate)
+  calculate.log(calculate)
   // return true for the browser
   return true;
 }
+
+
+  
