ebacd42b0a484ba61ce3ce09f6ee05c9a3c41bf8
ShtankoDaria <59530782+ShtankoDaria@users.noreply.github.com>
2020-02-07 14:35:17 +0100

    Update listener.js

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,5 +1,3 @@
-document.getElementById('calculate')
-  .addEventListener('click', function calculateHandler(event) {
-    console.log(event);
-  });
+document.getElementById('calculate-button')
+  .addEventListener('click', calculateHandler);
 
