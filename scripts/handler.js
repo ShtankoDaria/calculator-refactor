fdd79bc4059282cdfb1a61421aa355445f98b01c
ShtankoDaria <daria.shtanko@gmail.com>
2020-02-07 12:07:56 +0100

    Handler modif

--- scripts/handler.js
+++ scripts/handler.js
@@ -3,11 +3,20 @@ function calculateHandler(event) {
 
   var a = parseInt(document.querySelector('#value1').value);
   var b = parseInt(document.querySelector('#value2').value);
-  var operator = document.querySelector('#operator').value;
+  var operation = document.querySelector('#operator').value;
+  var calculate;
   // pass user input through core logic
-
+  if (typeof operation !== 'string') {
+    throw new Error('operation should be a string');
+  }
+  if (typeof a !== 'number') {
+    throw new Error('a should be a number');
+  }
+  if (typeof b !== 'number') {
+    throw new Error('b should be a number');
+  }
   // render output to DOM for user
-
+  document.querySelector('#result').innerHTML = calculate;
   // log user action for developers
 calculate.log(calculate)
   // return true for the browser
